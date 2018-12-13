37841286261ce3a237165ddddc930545e254ed52
ShiEr <tb3511791@163.com>
2018-12-13 15:38:19 +0800

    添加地图分布页面

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { Message } from 'element-ui'
 import { getToken } from '@/utils/auth'
 
+
 const service = axios.create({
   baseURL: process.env.BASE_API,
   timeout: 5000
